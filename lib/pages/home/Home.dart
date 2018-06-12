5f594cd2c851bc3aa4e163c4b9830cfe2462b5ab
chris <chris.j.field1992@gmail.com>
2018-06-12 23:03:56 +0100

    adding home page in.

--- lib/pages/home/Home.dart
+++ lib/pages/home/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../settings/AppSettings.dart';
 import '../../pageLayout/PageScaffold.dart';
 
 class Home extends StatelessWidget {
@@ -11,15 +12,49 @@ class Home extends StatelessWidget {
     );
   }
 
+  Widget _getLogo() {
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Image(
+        image: AssetImage('images/core/District_Logo.png'),
+        height: 250.0,
+      ),
+    );
+  }
+
+  Widget _getTitleRow(BuildContext context, String text) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          text,
+          style: Theme.of(context).textTheme.display1,
+        ),
+      ],
+    );
+  }
+
+  Widget _getDescription() {
+    return Padding(
+      padding: EdgeInsets.only(top: 30.0, left: 40.0, right: 20.0),
+      child: Text(
+        AppSettings.homeDescription,
+        style: TextStyle(
+          fontSize: 16.0,
+        ),
+      ),
+    );
+  }
+
   Widget _getHome(BuildContext context) {
     return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Text(
-            'hello world - Home!',
-            style: Theme.of(context).textTheme.display1,
-          ),
+          _getLogo(),
+          _getTitleRow(context, AppSettings.homeTitleLine1),
+          _getTitleRow(context, AppSettings.homeTitleLine2),
+          _getDescription(),
         ],
       ),
     );
--- lib/settings/AppSettings.dart
+++ lib/settings/AppSettings.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 class AppSettings {
   static const String appTitle = 'Nottingham District';
+  static const String homeTitleLine1 = 'Nottingham District';
+  static const String homeTitleLine2 = 'Bellringing';
+  static const String homeDescription = '''
+      Welcome to the app for bellringing in the Nottingham district of the Southwell & Nottingam Guid. 
+      You will find information on towers, recent performaces and upcoming events.
+      ''';
   static final ThemeData themeData = ThemeData(
     primarySwatch: Colors.blue,
     primaryColorDark: Colors.black,
